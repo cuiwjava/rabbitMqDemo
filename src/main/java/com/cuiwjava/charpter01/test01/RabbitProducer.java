860fd8a63005626305ef63cef10ba67786823747
cuiwei34 <cuiwei34@jd.com>
2020-05-27 19:21:38 +0800

    log

--- src/main/java/com/cuiwjava/charpter01/test01/RabbitProducer.java
+++ src/main/java/com/cuiwjava/charpter01/test01/RabbitProducer.java
@@ -3,6 +3,7 @@ package com.cuiwjava.charpter01.test01;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -23,7 +24,18 @@ public class RabbitProducer {
 
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-        //创建信道
+        // 创建一个type="direct" 持久化的,非自动删除的交换器
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct", true, false, null);
+        // 创建持久化、非排他的、非自动删除的队列
+        channel.queueDeclare(QUEUE_NAME,true,false,false,null);
+        //将交换器与队列通过路由键绑定
+        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
+        //发送一条持久化的消息
+        String message = "Hello World";
+        channel.basicPublish(EXCHANGE_NAME,ROUTING_KEY, MessageProperties.PERSISTENT_TEXT_PLAIN,message.getBytes());
+        // 关闭资源
+        channel.close();
+        connection.close();
 
     }
 }
